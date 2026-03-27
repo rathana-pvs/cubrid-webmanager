428d20288fbe72624430a8998e22472ca99195e8
rathana <varathana.tech@gmail.com>
2026-03-27 16:28:52 +0700

    Replace completely new UI to the existing:
    - update theme
    - update structure
    - update the whole styles

--- libs/api-interfaces/src/response/update-dbmt-user-client-response.ts
+++
@@ -1,6 +0,0 @@
-import { GetDbmtUserInfoClientResponse } from './get-dbmt-user-info-client-response';
-
-/**
- * Client response for updatedbmtuser (same as getdbmtuserinfo).
- */
-export type UpdateDbmtUserClientResponse = GetDbmtUserInfoClientResponse;
