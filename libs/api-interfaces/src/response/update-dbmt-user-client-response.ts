f74d429a7c62771d91ad66af257abaabd06d19e1
rathana <varathana.tech@gmail.com>
2026-03-27 16:38:48 +0700

    chore: restore required api interface file after refactor

---
+++ libs/api-interfaces/src/response/update-dbmt-user-client-response.ts
@@ -0,0 +1,6 @@
+import { GetDbmtUserInfoClientResponse } from './get-dbmt-user-info-client-response';
+
+/**
+ * Client response for updatedbmtuser (same as getdbmtuserinfo).
+ */
+export type UpdateDbmtUserClientResponse = GetDbmtUserInfoClientResponse;
