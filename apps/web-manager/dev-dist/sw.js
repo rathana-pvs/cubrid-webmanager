1b94679323e9e13310614aa79561d71916bddb3d
rathana <varathana.tech@gmail.com>
2026-04-03 11:30:15 +0700

    logo for install

--- apps/web-manager/dev-dist/sw.js
+++ apps/web-manager/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6d1fb45c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t7r9o4nivkc"
+    "revision": "0.i0egkhm3n7"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
